b5bde4b1c3197ed3fa9648ed960ff9ab53c94519
sleleu <leleu.sebastien@hotmail.fr>
2023-06-28 21:16:17 +0200

    add ft_strcpy.s

---
+++ ft_strcpy.s
@@ -0,0 +1,19 @@
+bits 64
+
+section .text
+global ft_strcpy
+
+ft_strcpy:
+	xor rax, rax ; i = 0
+.loop:
+	cmp byte [rsi + rax], 0; compare src and '\0'
+	je .return ; jump if equal to return
+	mov cl, [rsi + rax] ; move current byte in a 8 bits register
+	mov [rdi + rax], cl ; move the byte in the second argument (dest)
+	inc rax ; increment rax
+	jmp .loop ; jump to begin of loop
+
+.return:
+	mov byte [rdi + rax], 0 ; add '\0' in the end of dest
+	mov rax, rdi ; move dest in rax
+	ret ; return
