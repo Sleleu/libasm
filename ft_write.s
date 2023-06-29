9afcd9b4b273c37cf8f4b41eca21533e36ec27dd
Sebastien LELEU <leleu.sebastien@hotmail.fr>
2023-06-29 17:01:18 +0200

    add errno_location to ft_write.s

--- ft_write.s
+++ ft_write.s
@@ -1,5 +1,7 @@
 bits 64
 
+extern __errno_location
+
 section .text
 global ft_write
 
@@ -8,4 +10,14 @@ global ft_write
 ft_write:
 	mov rax, 1 ; syscall number of write
 	syscall
+	cmp rax, 0
+	jl .errno_error
+	ret
+
+.errno_error:
+	neg rax ; set rax to positive value
+	mov rdi, rax ; store rax as arg for errno
+	call __errno_location wrt ..plt ; set adress of errno var in rax, with respect to of processus linkage table
+	mov [rax], rdi ; stock rdi value in address of rax
+	mov rax, -1 ; set return value to -1
 	ret
